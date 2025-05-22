561cee5c9f4ca1b9452ce0a3f76ded64588127ad
Andrey <andreyantonovich100@gmail.com>
2025-05-23 00:08:40 +0300

    начинаю разбираться с css и <div>

--- hello_flask.py
+++ hello_flask.py
@@ -11,18 +11,19 @@ app.debug = True
 @app.route('/')  
 @app.route('/home', methods=["POST", "GET"]) 
 def all() -> 'html':
-    return render_template('index.html', the_title="ГЛАВНАЯ СТРАНЦА")
+    return render_template('main-page.html', the_title="ГЛАВНАЯ СТРАНЦА")
 
-@app.route("/code_result", methods=["POST"])
-def code_result() -> 'html':
-    #input_code = request.form["input_secret_code"]
 
-    log_request("User enter code:", 0) #, input_code
-    return render_template("code_result.html", the_title="МОИ ПИНЫ") #, the_code=input_code
+@app.route("/pins", methods=["POST"])
+def pins() -> 'html':
+    log_request("User enter code:", 0) 
+    return render_template("pins.html", the_title="МОИ ПИНЫ")
+
 
 @app.route("/films", methods=["POST"])
 def films() -> "html":
     return render_template("films.html", the_title="МОИ ЛЮБИМЫЕ ФИЛЬМЫ")
+
 
 @app.route("/viewlog")
 def viewlog() -> str:
