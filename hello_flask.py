e22868cf6c19d91c367befa518c121ec3b2ae980
Andrey <andreyantonovich100@gmail.com>
2025-05-28 19:45:02 +0300

    Реализовал чтение ссылок на статьи о фильмах из txt файла и парсинг обложек фильмов из статей на сайт

--- hello_flask.py
+++ hello_flask.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, url_for
+import wiki
 
 def log_request(log_message, req) -> None:
     with open("logs.txt", "a") as logs:
@@ -13,29 +14,25 @@ app.debug = True
 def all() -> 'html':
     return render_template('main-page.html', the_title="Главная страница")
 
-
-@app.route("/pins", methods=["POST"])
+@app.route("/pins", methods=["POST", "GET"])
 def pins() -> 'html':
     log_request("User enter code:", 0) 
     return render_template("pins.html", the_title="Мои пины")
 
 
-@app.route("/films", methods=["POST"])
+@app.route("/films", methods=["POST", "GET"])
 def films() -> "html":
-    return render_template("films.html", the_title="Мои любимые фильмы")
+    return render_template("films.html", the_title="Мои любимые фильмы", links=wiki.parse_film_info() )
 
-@app.route("/books", methods=["POST"])
+
+@app.route("/books", methods=["POST", "GET"])
 def books() -> "html":
     return render_template("books.html", the_title="Книги которые я прочел")
-
-
 @app.route("/viewlog")
 def viewlog() -> str:
     with open("logs.txt") as log:
         contents = log.read()   
-
     return contents
-
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ wiki.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+
+def parse_film_info():
+
+    """
+    Return list with films data. every film data is list with:  [film№,  wiki-url,   film-image, film-title]
+    """
+
+    with open("films-links.txt") as flims_links:
+        tmp = flims_links.read()                    #открыл и прочитал документ с ссылками на вики статьи о фильмах
+
+    lines_list= tmp.split()         #список с str ссылками  на статьи
+    counter = 0                     
+    films_data = []                 #список для записи данных о фильмах
+
+    for i in range(len(lines_list)):
+        counter += 1                #номер фильма в списке (начиная с 1)
+        url = lines_list[i]         #ссылка на фильм
+        resp = requests.get(url)    #resp.text вернет html код страницы в формате str                       
+        soup = BeautifulSoup(resp.text, "lxml")      #суп объект из которого можно вытянуть инфу с википедии
+
+        img = soup.find(class_="infobox-image").find(class_="mw-file-element").get("src")
+        title = soup.find(class_="infobox-above").string
+
+        films_data.append(["film_№" + str(counter), url, img, title])
+    return  (films_data)
