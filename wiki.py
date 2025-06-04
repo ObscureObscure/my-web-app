e62dc88f6d3b4055bafd950a1e1962784455ec55
Andrey <andreyantonovich100@gmail.com>
2025-06-04 13:58:32 +0300

    Версия сайта с марками, поправки в дизайн, исправлен путь до wiki.txt

--- wiki.py
+++ wiki.py
@@ -6,9 +6,17 @@ def parse_film_info():
     """
     Return list with films data. every film data is list with:  [film№,  wiki-url,   film-image, film-title]
     """
+    try:
+        with open("films-links.txt") as flims_links:
+            tmp = flims_links.read()                    #открыл и прочитал документ с ссылками на вики статьи о фильмах
+    except:
+        try:
+            with open("/home/ObscureObscure/mysite/static/films-links.txt") as flims_links:
+                tmp = flims_links.read() 
+        except:
+            with open("C:/Users/admin/Desktop/my-web-app/static/films-links.txt") as flims_links:
+                tmp = flims_links.read() 
 
-    with open("films-links.txt") as flims_links:
-        tmp = flims_links.read()                    #открыл и прочитал документ с ссылками на вики статьи о фильмах
 
     lines_list= tmp.split()         #список с str ссылками  на статьи
     counter = 0                     
